e6f66c18ac0d30d3786fa6e322ffebbc645417a6
pooria <bahremand@citynet.ir>
2025-03-30 15:11:52 +0330

    1

--- components/ui/add-conversation.jsx
+++ components/ui/add-conversation.jsx
@@ -1,46 +1,52 @@
+"use client";
 import * as React from "react";
 import { Dialog } from "radix-ui";
-import "./add-conversation.module.css";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import styles from "./add-conversation.module.css";
 
-const AddConversation = () => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <button className={`Button violet`}>Edit profile</button>
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="Overlay" />
-      <Dialog.Content className="Content">
-        <Dialog.Title className="Title">Edit profile</Dialog.Title>
-        <Dialog.Description className="Description">
-          Make changes to your profile here. Click save when you're done.
-        </Dialog.Description>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="name">
-            Name
-          </label>
-          <input className="Input" id="name" defaultValue="Pedro Duarte" />
-        </fieldset>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="username">
-            Username
-          </label>
-          <input className="Input" id="username" defaultValue="@peduarte" />
-        </fieldset>
-        <div
-          style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}
-        >
+const AddConversation = () => {
+  React.useEffect(() => {
+    console.log("rendered!");
+    console.log("styles.Overlay:", styles.Overlay);
+  }, []);
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className={`${styles.Button} violet`}>Add conversation</button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className={styles.Overlay} />
+        <Dialog.Content className={styles.Content}>
+          <Dialog.Title className={styles.Title}>Edit profile</Dialog.Title>
+          <Dialog.Description className={styles.Description}>
+            Make changes to your profile here. Click save when you're done.
+          </Dialog.Description>
+          <fieldset className={styles.Fieldset}>
+            <label className={styles.Label} htmlFor="email">
+              Email
+            </label>
+            <input className={styles.Input} id="email" />
+          </fieldset>
+          <div
+            style={{
+              display: "flex",
+              marginTop: 25,
+              justifyContent: "flex-end",
+            }}
+          >
+            <Dialog.Close asChild>
+              <button className={`${styles.Button} green`}>Save changes</button>
+            </Dialog.Close>
+          </div>
           <Dialog.Close asChild>
-            <button className={`Button green`}>Save changes</button>
+            <button className={styles.IconButton} aria-label="Close">
+              <Cross2Icon />
+            </button>
           </Dialog.Close>
-        </div>
-        <Dialog.Close asChild>
-          <button className="IconButton" aria-label="Close">
-            TEST
-          </button>
-        </Dialog.Close>
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
 
 export default AddConversation;
